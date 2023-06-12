4e687f2b5c8f472f8722d17aa814fa6bf44f9756
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-06-12 10:53:29 +0900

    create contract code

---
+++ contracts/ByMeATea.sol
@@ -0,0 +1,72 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract BuyMeATea {
+    // Event to tmit when a Memo is created
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    // Memo struct.
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    //List of all memos received from friends.
+    Memo[] memos;
+
+    // Address of contract deployer
+    address payable owner;
+
+    // Deploy logic.
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+
+    /**
+    * @dev buy a tea for contract owner
+    * @param _name name of the tea buyer
+    * @param _message a nice message from the tea buyer
+    */
+    function buyTea(string memory _name, string memory _message) public payable{
+        require(msg.value > 0, "can't buy tea wtih 0 eth");
+
+        // Add the memo to storage!
+         memos.push(Memo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+         ));
+
+         // Emit a log event when a new memo is created!
+         emit NewMemo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+         );
+    }
+
+    /**
+    * @dev send the entire balance stored in this contracts to the owner
+    */
+    function withDrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+
+    /**
+    * @dev retrive all the memos received and stored in this blockchain
+    */
+    function getMemos() public view returns (Memo[] memory) {
+        return memos;
+    }
+
+}
